541cbf9bbf3a1c3b21cc72b38862d2819ccc2b67
zeze <89672780+mdmdr8@users.noreply.github.com>
2022-11-13 21:22:03 +0900

    'deleteNewArr'

--- js/todo.js
+++ js/todo.js
@@ -1,18 +1,24 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
-const toDos = [];
+
+const TODOS_KEY = "todos";
+let toDos = [];
 
 // delete to-do list
 function deleteTodo(event) {
   const li = event.target.parentElement;
   li.remove();
+  //   li.id는 string
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paintTodo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
 
   const button = document.createElement("button");
   button.innerText = "x";
@@ -28,13 +34,26 @@ function todoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintTodo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintTodo(newTodoObj);
   saveToDos();
 }
 
 function saveToDos() {
-  localStorage.setItem("todos", toDos);
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 toDoForm.addEventListener("submit", todoSubmit);
+
+//db에 json형으로 저장하여 저장된 정보 화면에 뿌리기
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintTodo);
+}
